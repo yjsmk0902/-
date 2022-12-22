5c0ee9e9bba560451f201dc6d0b3936b8784680a
yjsmk0902 <yjsmk0902@gmail.com>
2022-12-22 10:08:48 +0900

    첫커밋

---
+++ Spring Project/Next/src/main/java/Next/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Next.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
